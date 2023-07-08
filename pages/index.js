1ba98cf392fb41e30266479448965cc618295c1b
Jonathas Soares <joul.gs@gmail.com>
2023-07-08 12:27:02 +0000

    add initial page

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+function Home(){
+    return (
+        <div>Oi mozim, já posso comer o bolo?
+            <br/>
+            <button>Sim</button>{' '}
+            <button>Com Certeza</button>
+        </div>
+    )
+}
+
+export default Home;
