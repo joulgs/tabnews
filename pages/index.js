c197d2b933385355f924e9125982be6132d0c2df
Jonathas Soares <joul.gs@gmail.com>
2023-07-18 12:46:46 +0000

    add scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,11 @@
-function Home(){
-    return (
-        <div>Oi mozim, já posso comer o bolo?
-            <br/>
-            <button>Sim</button>{' '}
-            <button>Com Certeza</button>
-        </div>
-    )
+function Home() {
+  return (
+    <div>
+      Oi mozim, já posso comer o bolo?
+      <br />
+      <button>Sim</button> <button>Com Certeza</button>
+    </div>
+  );
 }
 
 export default Home;
